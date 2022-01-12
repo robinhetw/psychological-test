bd951ab69cdd168be490ce8a82e85f73a151040a
Robin He <31844669+robinhetw@users.noreply.github.com>
2022-01-12 17:51:52 +0800

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -181,4 +181,37 @@ window.onload = function () {
   $("#negative").html(answerNegative + "<span>%</span>");
   $("#emotional").html(answerEmotional + "<span>%</span>");
   $("#indifferent").html(answerIndifferent + "<span>%</span>");
+
+  var ctx = document.getElementById("chart").getContext("2d");
+
+  var chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["神經質父母", "消極型父母", "情緒化父母", "冷漠型父母"],
+      datasets: [
+        {
+          label: "# 百分比",
+          data: [
+            answerNeuroticism,
+            answerNegative,
+            answerEmotional,
+            answerIndifferent,
+          ],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255,99,132,1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
 };
